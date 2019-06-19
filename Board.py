7254e3177464b154d4dcd986a51d90c4167a39e8
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-19 17:50:52 +0100

    Added function to place pieces at start positions
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Board.py
+++ Board.py
@@ -20,35 +20,73 @@ class Board:
     def create_canvas(self):
         self.canvas = Canvas(self.master, width=self.max_size, height=self.max_size)
         self.canvas.pack()
-        self.canvas.configure(background='black')
         for i in range(self.n):
             for j in range(self.n):
-                self.canvas.create_rectangle(i * self.cell_size, j * self.cell_size,
-                                             (i + 1) * self.cell_size, (j + 1) * self.cell_size,
-                                             fill=self.cells[i][j])
+                if (i + j) % 2 == 1:
+                    self.canvas.create_rectangle(i * self.cell_size, j * self.cell_size,
+                                                 (i + 1) * self.cell_size, (j + 1) * self.cell_size,
+                                                 fill='black')
 
     def create_board(self):
         cells = []
         for i in range(self.n):
             cells.append([])
             for j in range(self.n):
-                if (i + j) % 2 == 0:
+                if (i + j) % 2 == 1:
                     cells[i].append('black')
                 else:
                     cells[i].append('white')
         return cells
 
     def place_piece(self, piece, x, y):
-        board_x = x * self.cell_size + self.cell_size // 2
-        board_y = y * self.cell_size + self.cell_size // 2
-        self.canvas.create_text(board_x, board_y, text=piece.char, font='Verdana 20')
+        board_x = (x - 1) * self.cell_size + self.cell_size // 2
+        board_y = (y - 1) * self.cell_size + self.cell_size // 2
+        if (x + y) % 2 == 1:
+            pad = 27
+            self.canvas.create_rectangle(board_x - pad, board_y - pad, board_x + pad, board_y + pad, fill='white')
+        self.canvas.create_text(board_x, board_y, text=piece.char, font='Arial 40', tag='piece')
+
+    def start_setup(self):
+        pieces = []
+        # pawns
+        for i in range(self.n):
+            pieces.append(Piece('white', 'pawn', i + 1, 7))
+            pieces.append(Piece('black', 'pawn', i + 1, 2))
+
+        # rooks
+        pieces.append(Piece('white', 'rook', 1, 8))
+        pieces.append(Piece('white', 'rook', 8, 8))
+        pieces.append(Piece('black', 'rook', 1, 1))
+        pieces.append(Piece('black', 'rook', 8, 1))
+
+        # knights
+        pieces.append(Piece('white', 'knight', 2, 8))
+        pieces.append(Piece('white', 'knight', 7, 8))
+        pieces.append(Piece('black', 'knight', 2, 1))
+        pieces.append(Piece('black', 'knight', 7, 1))
+
+        # bishops
+        pieces.append(Piece('white', 'bishop', 3, 8))
+        pieces.append(Piece('white', 'bishop', 6, 8))
+        pieces.append(Piece('black', 'bishop', 3, 1))
+        pieces.append(Piece('black', 'bishop', 6, 1))
+
+        # queens
+        pieces.append(Piece('white', 'queen', 4, 8))
+        pieces.append(Piece('black', 'queen', 4, 1))
+
+        # kings
+        pieces.append(Piece('white', 'king', 5, 8))
+        pieces.append(Piece('black', 'king', 5, 1))
+
+        for p in pieces:
+            self.place_piece(p, p.x, p.y)
 
 
 def main():
     root = Tk()
     b = Board(root, 8)
-    p = Piece('black', 'king')
-    b.place_piece(p, 1, 1)
+    b.start_setup()
     root.mainloop()
 
 
--- Piece.py
+++ Piece.py
@@ -3,9 +3,11 @@ from tkinter import ttk
 
 
 class Piece:
-    def __init__(self, colour, piece):
+    def __init__(self, colour, piece, x=None, y=None):
         self.colour = colour
         self.piece = piece
+        self.x = x
+        self.y = y
         self.char_dict = {
             'king': {'white': u'\u2654', 'black': u'\u265A'},
             'queen': {'white': u'\u2655', 'black': u'\u265B'},
