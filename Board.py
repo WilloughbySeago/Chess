d750178b40493d95b65675f9528d853dfa9b47e0
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-20 15:29:23 +0100

    Finished pawn checking
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Board.py
+++ Board.py
@@ -4,15 +4,15 @@ from Piece import *
 
 
 class Board:
-    def __init__(self, master, n):
+    """This is a class for a chess board"""
+    def __init__(self, master, n: int):
         self.master = master
         self.n = n
         self.master.geometry('1250x600+10+10')
         self.master.title('Board')
-        self.master.update_idletasks()
-        self.max_size = min(self.master.winfo_width(), self.master.winfo_height())
-        self.master.update_idletasks()
-        self.cell_size = self.max_size // self.n
+        self.master.update_idletasks()  # needed for next line to work
+        self.max_size = min(self.master.winfo_width(), self.master.winfo_height())  # set board size
+        self.cell_size = self.max_size // self.n  # calculate the size of one square
         self.cells = []
         self.entries = None
         self.start_dict = None
@@ -27,28 +27,39 @@ class Board:
         self.create_start()
         self.positions = self.start_dict.copy()
         self.controls()
+        self.draw()
 
-        self.master.bind('<Return>', lambda e: self.redraw())
+        self.master.bind('<Return>', lambda e: self.redraw())  # hit enter to submit
+
+        self.style = ttk.Style()
+        self.colour = '#444444'
+        self.style.configure('TFrame', background=self.colour)
+        self.style.configure('TButton', background=self.colour)
+        self.style.configure('TEntry', background=self.colour)
+        self.master.configure(background=self.colour)
 
     def create_canvas(self):
+        """Create the canvas and draw the black squares"""
         self.canvas = Canvas(self.canvas_frame, width=self.max_size, height=self.max_size)
         self.canvas.pack()
         for i in range(self.n):
             for j in range(self.n):
-                if (i + j) % 2 == 1:
+                if (i + j) % 2 == 1:  # every other square is black
                     self.canvas.create_rectangle(i * self.cell_size, j * self.cell_size,
                                                  (i + 1) * self.cell_size, (j + 1) * self.cell_size,
                                                  fill='black')
 
     def place_piece(self, piece, x, y):
+        """Draw given piece at given coords"""
         board_x = (x - 1) * self.cell_size + self.cell_size // 2
         board_y = (y - 1) * self.cell_size + self.cell_size // 2
-        if (x + y) % 2 == 1:
+        if (x + y) % 2 == 1:  # if the square is black draw a white box for the piece to go in
             pad = 27
             self.canvas.create_rectangle(board_x - pad, board_y - pad, board_x + pad, board_y + pad, fill='white')
         self.canvas.create_text(board_x, board_y, text=piece.char, font='Arial 40', tag='piece')
 
     def start_setup(self):
+        """Create an array of all the squares and also the dict of the start positions"""
         for i in range(self.n):
             self.cells.append([])
             for j in range(self.n):
@@ -60,6 +71,7 @@ class Board:
                     self.start_dict[f'{i}{j}'] = None
 
     def create_start(self):
+        """Create the dictionary of start positions leaving all other squares as None"""
         self.start_dict = {}
 
         # pawns
@@ -84,16 +96,27 @@ class Board:
         self.start_dict[f'{5}{8}'] = Piece('white', 'king', 5, 8)
         self.start_dict[f'{5}{1}'] = Piece('black', 'king', 5, 1)
 
+        # create all other positions as None
+        for i in range(self.n):
+            for j in range(self.n):
+                try:
+                    self.start_dict[f'{i+1}{j+1}']  # if it doesn't exist it will throw a key error
+                except KeyError:
+                    self.start_dict[f'{i+1}{j+1}'] = None  # if the key error is thrown set it to None
+
     def draw(self):
-        self.canvas.destroy()
+        """draw the board and pieces"""
+        self.canvas.destroy()  # kill the canvas and then draw it again
         self.create_canvas()
 
+        # draw all the pieces again
         for key in self.positions.keys():
             p = self.positions[key]
             if p is not None:
                 self.place_piece(p, p.x, p.y)
 
     def redraw(self):
+        """draw the board and add new pieces from the entries"""
         self.canvas.destroy()
         self.create_canvas()
 
@@ -101,36 +124,44 @@ class Board:
             for j in range(self.n):
                 entry = self.entries[i][j]
                 value = entry.get()
+                # a blank entry is no piece
                 if value == '':
                     self.positions[f'{i + 1}{j + 1}'] = None
                     continue
+                # min value length is 7 as the format is color piece and black/white is 5 letters
+                # + space + first letter of piece gives 7 characters at least
                 if len(value) < 7:
                     messagebox.showerror(title='Invalid Entry',
                                          message=f'Invalid Entry:\n({i + 1},{j + 1})\nEnter \'colour type\'')
-                    print('value')
+                    # print('value')
                     continue
+                # check that the first 5 letters are either black or white
                 if value[:5].lower() in ['black', 'white']:
                     colour = value[:5]
                 else:
                     messagebox.showerror(title='Invalid Colour',
                                          message=f'Invalid Entry:\n({i + 1},{j + 1})\nEnter \'black\' or \'white\'')
-                    print('value')
+                    # print('value')
                     continue
+                # check that from 6 on it is a valid piece
                 if value[6:].lower() in ['pawn', 'rook', 'knight', 'bishop', 'queen', 'king']:
                     piece = value[6:]
                 else:
                     messagebox.showerror(title='Invalid Type',
                                          message=f'Invalid Entry:\n({i + 1},{j + 1})\nEnter \'type\' eg \'rook\'')
-                    print('value')
+                    # print('value')
                     continue
+                # update dict of piece positions
                 self.positions[f'{i + 1}{j + 1}'] = Piece(colour, piece, i + 1, j + 1)
 
+        # draw pieces
         for key in self.positions.keys():
             p = self.positions[key]
             if p is not None:
                 self.place_piece(p, p.x, p.y)
 
     def clear(self):
+        """clear all entry fields and the board"""
         for i in range(self.n):
             for j in range(self.n):
                 entry = self.entries[i][j]
@@ -138,6 +169,7 @@ class Board:
         self.redraw()
 
     def controls(self):
+        """Set up entry fields/buttons etc."""
         self.entries = []
         for i in range(self.n):
             self.entries.append([])
@@ -151,18 +183,19 @@ class Board:
                 try:
                     piece = self.positions[f'{i + 1}{j + 1}']
                     entry.insert(0, f'{piece.colour} {piece.piece}')
-                except KeyError:
+                except KeyError:  # if the position hasn't been added to the dict (should no longer happen)
                     entry.delete(0, END)
+                except AttributeError:  # if piece is None then piece.colour raises this error
+                    pass
 
-        ttk.Button(self.controls_frame, text='Submit', command=self.redraw).place(x=568, y=550)  # relx=-200, rely=550)
+        # add submit and clear buttons to the controls
+        ttk.Button(self.controls_frame, text='Submit', command=self.redraw).place(x=568, y=550)
         ttk.Button(self.controls_frame, text='Clear', command=self.clear).place(x=0, y=550)
 
 
 def main():
     root = Tk()
-    b = Board(root, 8)
-    b.start_setup()
-    b.draw()
+    b = Board(root, 8)  # create the board
     root.mainloop()
 
 
--- Move_checker.py
+++ Move_checker.py
@@ -2,21 +2,26 @@ from Board import *
 
 
 class MoveChecker(Board):
+    """This is a class derived from the Board class with extra functions to check the legal moves a piece has"""
+
     def __init__(self, master, n):
-        Board.__init__(self, master, n)
+        Board.__init__(self, master, n)  # call to Board.__init__ to create board
         self.master = master
         self.n = n
-        self.entry = ttk.Entry(self.controls_frame)
+        self.entry = ttk.Entry(self.controls_frame)  # add the entry field to specify which pieces to check
         self.entry.place(x=200, y=550)
         self.label = ttk.Label(self.controls_frame, text='Enter row and column number '
-                                                         'in format xy, \'all\' or \'colour\'')
+                                                         'in format xy, \'all\' or \'colour\'')  # add instruction label
         self.label.place(x=170, y=575)
+        # add button to check current selection
         ttk.Button(self.controls_frame, text='Check', command=self.what_to_check).place(x=350, y=550)
+        self.draw()
 
     def what_to_check(self):
+        """Get user input from entry to choose what pieces to check"""
         value = self.entry.get()
 
-        if len(value) not in [2, 3, 5]:
+        if len(value) not in [2, 3, 5]:  # allowed inputs are xy or all or black/white hence the allowed lengths
             messagebox.showerror(title='Check error', message='Invalid format, enter either:'
                                                               '\n-Coordinates in format \'xy\''
                                                               '\n-The word \'all\''
@@ -65,36 +70,47 @@ class MoveChecker(Board):
                                                                 '\n-The word \'all\''
                                                                 '\n-\'black\' or \'white\'')
             return
-        if x is not None:
+        if mode == 'xy':
             self.get_check_list(mode, x=x, y=y)
         else:
             self.get_check_list(mode)
 
     def get_check_list(self, mode, x=0, y=0):
+        """Depending on the mode get list of squares containing the pieces to check"""
         to_check = []
         if mode == 'xy':
+            try:
+                self.positions[f'{x}{y}']
+            except KeyError:
+                pass
             to_check.append((x, y))
+            # nice and simple just check the given point contains a piece and add it to the list
 
         elif mode == 'all':
+            # iterate through all squares and any that have a piece are added to the list
             for i in range(self.n + 1):
                 for j in range(self.n + 1):
                     try:
                         if self.positions[f'{i}{j}'] is not None:
                             to_check.append((i, j))
                     except KeyError:
+                        # if self.positions['ij'] hasn't been created yet
+                        # this will be raised (should not happen anymore)
                         pass
 
         elif mode == 'white':
+            # iterate through squares and if it contains a white piece add it to the list
             for i in range(self.n + 1):
                 for j in range(self.n + 1):
                     try:
                         if self.positions[f'{i}{j}'] is not None:
                             if self.positions[f'{i}{j}'].colour == 'white':
                                 to_check.append((i, j))
-                    except KeyError:
+                    except KeyError:  # shouldn't happen as should be initialised to None
                         pass
 
         elif mode == 'black':
+            # see comments above for mode == 'white'
             for i in range(self.n + 1):
                 for j in range(self.n + 1):
                     try:
@@ -105,51 +121,96 @@ class MoveChecker(Board):
                         pass
 
         for p in to_check:
+            # print values to check (for debugging purposes)
             print(self.positions[f'{p[0]}{p[1]}'])
             self.check(p)
 
         for p in to_check:
-            self.canvas.create_oval(p[0] * self.cell_size - self.cell_size // 2 + 10,
-                                    p[1] * self.cell_size - self.cell_size // 2 + 10,
-                                    p[0] * self.cell_size - self.cell_size // 2 - 10,
-                                    p[1] * self.cell_size - self.cell_size // 2 - 10, fill='yellow')
+            # draw a circle over every piece to be checked
+            r = 10  # radius
+            self.canvas.create_oval(p[0] * self.cell_size - self.cell_size // 2 + r,
+                                    p[1] * self.cell_size - self.cell_size // 2 + r,
+                                    p[0] * self.cell_size - self.cell_size // 2 - r,
+                                    p[1] * self.cell_size - self.cell_size // 2 - r, fill='yellow')
 
     def check(self, p):
+        """p is a point (x, y) check the possible moves of the point at p"""
         piece = self.positions[f'{p[0]}{p[1]}']
         allowed = []
-
+        x, y = p  # extract x and y values from point
         # if the piece is a pawn check its possible spots
         if piece.piece == 'pawn':
             if piece.colour == 'white':
-                point = (p[0], p[1] - 1)  # can always move 1 forward
+                point = (x, y - 1)  # can always move 1 forward
                 if self.is_empty(point, 'white'):
                     allowed.append(point)
                 if piece.y == 7:
-                    point = (p[0], p[1] - 2)  # can move 2 forward the first time they move
-                    if self.is_empty(point, 'white'):
+                    point = (x, y - 2)  # can move 2 forward the first time they move
+                    if self.is_empty(point, 'white') and self.is_empty((x, y - 1), 'white') and self.is_empty(
+                            (x, y - 1), 'black'):  # check that the point to move to and the point between are valid
                         allowed.append(point)
+                point = (x - 1, y - 1)  # check diagonals, if there is a black piece then it is a valid spot
+                try:
+                    contents = self.positions[f'{x - 1}{y - 1}']
+                except KeyError:  # should no longer happen
+                    pass
+                else:
+                    if contents is not None:
+                        if contents.colour == 'black':
+                            allowed.append(point)
+                point = (x + 1, y - 1)
+                try:
+                    contents = self.positions[f'{x + 1}{y - 1}']
+                except KeyError:  # should no longer happen
+                    pass
+                else:
+                    if contents is not None:
+                        if contents.colour == 'black':
+                            allowed.append(point)
 
             else:
-                point = (p[0], p[1] + 1)  # can always move 1 forward
+                point = (x, y + 1)  # can always move 1 forward
                 if self.is_empty(point, 'black'):
                     allowed.append(point)
                 if piece.y == 2:
-                    point = (p[0], p[1] + 2)  # can move 2 forward the first time the move
-                    if self.is_empty(point, 'black'):
+                    point = (x, y + 2)  # can move 2 forward the first time the move
+                    if self.is_empty(point, 'black') and self.is_empty((x, y + 1), 'black') and self.is_empty(
+                            (x, y + 1), 'white'):  # check that the point to move to and the point between are valid
                         allowed.append(point)
+                point = (x - 1, y + 1)  # check diagonals, if there is a white piece then it is a valid spot
+                try:
+                    contents = self.positions[f'{x - 1}{y + 1}']
+                except KeyError:  # should no longer happen
+                    pass
+                else:
+                    if contents is not None:
+                        if contents.colour == 'white':
+                            allowed.append(point)
+                point = (x + 1, y + 1)
+                try:
+                    contents = self.positions[f'{x + 1}{y + 1}']
+                except KeyError:  # should no longer happen
+                    pass
+                else:
+                    if contents is not None:
+                        if contents.colour == 'white':
+                            allowed.append(point)
 
         for p in allowed:
-            self.canvas.create_oval(p[0] * self.cell_size - self.cell_size // 2 + 10,
-                                    p[1] * self.cell_size - self.cell_size // 2 + 10,
-                                    p[0] * self.cell_size - self.cell_size // 2 - 10,
-                                    p[1] * self.cell_size - self.cell_size // 2 - 10, fill='red')
+            # draw a red circle on all spots the piece(s) could move to
+            r = 15  # radius
+            self.canvas.create_oval(p[0] * self.cell_size - self.cell_size // 2 + r,
+                                    p[1] * self.cell_size - self.cell_size // 2 + r,
+                                    p[0] * self.cell_size - self.cell_size // 2 - r,
+                                    p[1] * self.cell_size - self.cell_size // 2 - r, fill='red')
 
     def is_empty(self, point, colour):
+        """Check if a square is either empty or contains a piece of the opposite colour that can be taken"""
         try:
             contents = self.positions[f'{point[0]}{point[1]}']  # get contents of square
-            if contents is None:  # in there is nothing there
+            if contents is None:  # there is nothing there
                 return True
-            if contents.colour == colour:  # if the piece there in the same colour
+            if contents.colour == colour:  # if the piece there in the same colour then the spot isn't valid
                 return False
             else:
                 return True  # if the piece is the opposite colour then a move there is valid
@@ -159,7 +220,7 @@ class MoveChecker(Board):
 
 def main():
     root = Tk()
-    move_checker = MoveChecker(root, 8)
+    move_checker = MoveChecker(root, 8)  # create the game
     root.mainloop()
 
 
